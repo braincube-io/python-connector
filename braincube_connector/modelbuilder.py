54e97febb511de456d891e5fac27c30929a634d6
Alexis TRESCARTE <alexis.trescarte@braincube.com>
2022-04-20 09:05:12 +0200

    create ModelBuilder

--- braincube_connector/modelbuilder.py
+++ braincube_connector/modelbuilder.py
@@ -1,10 +1,24 @@
 from typing import List, Dict
 
+import requests
+
+from braincube_connector import client
 from braincube_connector.bases import base
+from braincube_connector.client import request_ws
 from braincube_connector.memory_base.nested_resources.period import Period
 from braincube_connector.memory_base.nested_resources.variable import VariableDescription
 
 
-class ModelBuilder(base.Base):
+class ModelBuilder(client.Client):
 
-    def create_study(self, name: str, target: VariableDescription, period: Period, variables: List[VariableDescription], description: str = '',  conditions: List[Dict] = [],  ):
+    @staticmethod
+    def create_study(self, name: str, memorybase: MemoryBase, target: VariableDescription, period: Period, variables: List[VariableDescription],
+                     description: str = '', conditions: List[Dict] = [], ):
+        path = "?"
+        header = "?"
+        body_data = {
+            "name": name,
+            "description": description,
+            "memoryBase": memorybase
+        }
+        study = self.request_ws(path=path, headers=header, body_data=body_data)
