e742dfd47c73c589ff3f5fe7eda07005c9b1741b
AlexisTres <81624964+AlexisTres@users.noreply.github.com>
2024-09-04 13:12:17 +0000

    fix flake8

--- braincube_connector/data/data.py
+++ braincube_connector/data/data.py
@@ -76,7 +76,7 @@ def _extract_format_data(raw_dataset: Dict[str, Any]) -> Dict[int, Any]:
         col_id = int(col["id"].split("/d")[1])
 
         if DATACOL not in col:
-            warnings.warn("No data found for: " + col["id"])
+            warnings.warn(f"No data found for: {col['id']}", stacklevel=2)
             continue
 
         col_data = col[DATACOL]
